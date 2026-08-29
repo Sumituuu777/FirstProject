e29cdcf5cd1aa04648b8410dac1eac90826d8789
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-08-29 20:42:22 +0530

    Using stack navigation in react native

--- App.jsx
+++ App.jsx
@@ -1,11 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
 
+const Stack =createNativeStackNavigator()
+
+const StackNavigator=()=>{
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home}/>
+      <Stack.Screen name='Profile' component={Profile}/>
+      <Stack.Screen name='Search' component={Search}/>
+    </Stack.Navigator>
+  )
+}
 const App = () => {
   return (
-    <View>
-      <Text>Hello React Native 🚀</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   )
 }
 
---
+++ src/screens/Profile.jsx
@@ -0,0 +1,15 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = ({navigation}) => {
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Button title='Go to Search' onPress={()=>navigation.navigate("Search")}/>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
